0fbcd8b6277404df60b814bbb630e421a80c0f4b
Alexandra <alexandracalonico@gmail.com>
2025-04-22 23:42:43 +0200

    fix

--- src/main/java/bts/LabLune/servicio/DoctorServicio.java
+++ src/main/java/bts/LabLune/servicio/DoctorServicio.java
@@ -1,47 +1,36 @@
 package bts.LabLune.servicio;
 
 import bts.LabLune.modelo.Doctor;
-import bts.LabLune.modelo.Patient;
 import bts.LabLune.repositorio.DoctorRepositorio;
-import bts.LabLune.repositorio.PacienteRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Service
-public class DoctorServicio  implements IDoctorServicio{
-
+//para que sea componente de fabrica de spring y pueda hacer uso de spring
+public class DoctorServicio implements IDoctorServicio {
     @Autowired
+    //Inyectar de manera automatico
     private DoctorRepositorio doctorRepositorio;
 
     @Override
-
     public List<Doctor> listarDoctores() {
-        return doctorRepositorio.findAll();  // Esto debería devolver todos los doctores desde la base de datos
-    }
-    
-
-    @Override
-    public Doctor buscarDoctoresporId(Integer idDoctor) {
-        return null;
+        return doctorRepositorio.findAll();
     }
 
     @Override
-    public void guardarDoctor(Doctor idDoctor) {
+    public Doctor buscarDoctoresporId(Integer idResult) {
+        return doctorRepositorio.findById(idResult).orElse(null);
+    }
 
+    @Override
+    public void guardarDoctor(Doctor doctor) {
+        doctorRepositorio.save(doctor);
     }
 
     @Override
     public void eliminarDoctor(Doctor doctor) {
-
+        doctorRepositorio.delete(doctor);
     }
 }
-
